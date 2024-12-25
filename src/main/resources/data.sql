5a338c94c56ea8b213532a09c8344a1645c8eb5b
ukyoda <3340595+ukyoda@users.noreply.github.com>
2024-12-25 23:00:09 +0900

    fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
+
+-- users data
+insert into users (email, password, name, role_type) values ('admin@example.com', 'admin', 'admin', 'ADMIN');
+
+-- books data
 insert into books (title, author, release_date, created_at, updated_at) values ('なんかの本1', 'なんかの作者', 1735052400, 1735052400, 1735052400);
