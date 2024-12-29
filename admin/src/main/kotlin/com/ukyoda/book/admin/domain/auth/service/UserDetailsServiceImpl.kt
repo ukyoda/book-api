681f4eaa304aee65e1b42c4e248667ade97b65d7
ukyoda <3340595+ukyoda@users.noreply.github.com>
2024-12-29 16:05:13 +0900

    ADMINだけログインできるように

--- admin/src/main/kotlin/com/ukyoda/book/admin/domain/auth/service/UserDetailsServiceImpl.kt
+++ admin/src/main/kotlin/com/ukyoda/book/admin/domain/auth/service/UserDetailsServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.ukyoda.book.admin.domain.auth.service
 
+import com.ukyoda.book.common.domain.user.model.RoleType
 import com.ukyoda.book.common.domain.user.repository.UserRepository
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
@@ -13,8 +14,7 @@ class UserDetailsServiceImpl(
 ) : UserDetailsService {
     override fun loadUserByUsername(email: String): UserDetails {
         val user =
-            userRepository.findByEmail(email) ?: throw UsernameNotFoundException("User not found")
-
+            userRepository.findByEmail(email)?.takeIf { it.roleType == RoleType.ADMIN } ?: throw UsernameNotFoundException("User not found")
         return User
             .withUsername(user.email)
             .password(user.password)
