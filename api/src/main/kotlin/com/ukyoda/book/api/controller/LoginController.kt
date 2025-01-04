412afaf048b8bf88680670634ab29a6bf83478b6
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-04 22:27:33 +0900

    fix

--- api/src/main/kotlin/com/ukyoda/book/api/controller/LoginController.kt
+++ api/src/main/kotlin/com/ukyoda/book/api/controller/LoginController.kt
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController
 @ApiPrefix
 @CrossOrigin
 class LoginController(
-    private val daoAuthenticationProvider: DaoAuthenticationProvider
+    private val daoAuthenticationProvider: DaoAuthenticationProvider,
     private val jwtComponent: JwtComponent
 ) {
     @PostMapping("/login")
