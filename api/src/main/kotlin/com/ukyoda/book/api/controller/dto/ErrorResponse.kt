a0665b4f96294ec6e6168aa8dbaafff6c0e2ae62
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-12 19:15:00 +0900

    fix

--- api/src/main/kotlin/com/ukyoda/book/api/controller/dto/ErrorResponse.kt
+++ api/src/main/kotlin/com/ukyoda/book/api/controller/dto/ErrorResponse.kt
@@ -1,6 +1,7 @@
 package com.ukyoda.book.api.controller.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
 
 data class ErrorResponse(
     @JsonProperty("code")
@@ -9,4 +10,9 @@ data class ErrorResponse(
     val reason: String,
     @JsonProperty("message")
     val message: String? = null,
-)
+) {
+    fun toJson(): String =
+        ObjectMapper()
+            .writerWithDefaultPrettyPrinter()
+            .writeValueAsString(this)
+}
