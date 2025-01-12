d4b09feeb8232704336c21af59c1059f0bc5085b
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-12 21:49:34 +0900

    add dotenv

--- api/src/main/kotlin/com/ukyoda/book/ApiApplication.kt
+++ api/src/main/kotlin/com/ukyoda/book/ApiApplication.kt
@@ -1,11 +1,19 @@
 package com.ukyoda.book
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.function.Consumer
 
 @SpringBootApplication
 class ApiApplication
 
 fun main(args: Array<String>) {
+    val dotenv = Dotenv.load()
+    dotenv.entries().forEach(
+        Consumer { entry ->
+            System.setProperty(entry.key, entry.value)
+        },
+    )
     runApplication<ApiApplication>(*args)
 }
