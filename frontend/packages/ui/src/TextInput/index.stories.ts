212f186b7c76c23b7bf6d5fbbe1135d045f26531
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-25 19:44:18 +0900

    fix

---
+++ frontend/packages/ui/src/TextInput/index.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import TextInput from "./index.vue";
+
+type Story = StoryObj<typeof meta>;
+const meta = {
+  title: "@repo/ui/TextInput",
+  component: TextInput,
+  tags: ["autodocs"],
+} satisfies Meta<typeof TextInput>;
+
+export default meta;
+
+export const Normal: Story = {
+  args: {
+    label: "Label",
+  },
+};
