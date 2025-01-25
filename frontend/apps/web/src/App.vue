dc6cfd317d8c48b66fa3d42d854cffc9d30f1294
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-25 19:05:51 +0900

    fix

--- frontend/apps/web/src/App.vue
+++ frontend/apps/web/src/App.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
 import { TextInput } from "@repo/ui/TextInput";
+import { toTypedSchema } from "@vee-validate/zod";
+import { computed, ref } from "vue";
+import { z } from "zod";
+
+const validator = computed(() => z.string().min(5).max(20));
+const value = ref("");
 </script>
 <template>
   <div>test</div>
-  <TextInput type="text" />
+  <div class="p-4">
+    <TextInput
+      v-model="value"
+      type="text"
+      label="Text Field"
+      :validation-schema="toTypedSchema(validator)"
+    />
+    <p>{{ value }}</p>
+  </div>
 </template>
--- frontend/packages/ui/src/TextInput/index.vue
+++ frontend/packages/ui/src/TextInput/index.vue
@@ -1,10 +1,65 @@
-<script setup lang="ts">
-const model = defineModel();
-defineProps<{
-  type?: "text" | "number" | "password";
+<script setup lang="ts" generic="T extends string | number">
+import { XCircleIcon } from "@heroicons/vue/16/solid";
+import { useField, type TypedSchema } from "vee-validate";
+import { defineEmits, computed, useId, type InputHTMLAttributes } from "vue";
+const model = defineModel<T>();
+const props = defineProps<{
+  type?: InputHTMLAttributes["type"];
+  id?: string;
+  name?: string;
+  label?: string;
+  validationSchema?: TypedSchema;
 }>();
+defineEmits<{
+  click: [value: T];
+}>();
+
+const formId = useId();
+const nameRef = computed(() => props.name ?? `text-input-${formId}`);
+const idRef = computed(() => props.id ?? `text-input-${formId}`);
+
+const { errorMessage, value: fieldValue } = useField(
+  nameRef.value,
+  props.validationSchema,
+  {
+    initialValue: model.value,
+    syncVModel: true,
+  },
+);
+const isError = computed(() => errorMessage.value?.length);
+const clear = () => {
+  fieldValue.value = "";
+};
+const fontSize = "text-sm font-medium";
 </script>
 
 <template>
-  <input :type="type" v-model="model" class="bg-gray-300 p-3 font-bold" />
+  <div class="flex flex-col">
+    <label
+      v-if="label"
+      :for="idRef"
+      :class="[fontSize, 'text-sm font-medium text-gray-700']"
+    >
+      {{ label }}
+    </label>
+    <div
+      class="border-[1px] rounded-md flex px-2.5"
+      :class="[isError ? 'border-red-600' : 'border-gray-300']"
+    >
+      <input
+        v-model="fieldValue"
+        :type="type"
+        :id="idRef"
+        :name="nameRef"
+        class="py-2 focus:outline-none flex-1"
+      />
+      <button v-if="(fieldValue?.length ?? 0) > 0" type="button" @click="clear">
+        <span class="sr-only">Clear</span>
+        <XCircleIcon class="size-[1.2rem] text-gray-500" />
+      </button>
+    </div>
+    <div v-if="errorMessage" :class="[fontSize, 'text-red-600']">
+      {{ errorMessage }}
+    </div>
+  </div>
 </template>
