3f79bb2d96d1186251ad3fce81e5de87412d251e
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-25 17:57:23 +0900

    fix

--- frontend/packages/ui/src/TextInput/index.vue
+++ frontend/packages/ui/src/TextInput/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 const model = defineModel();
 defineProps<{
-  type: "text" | "number" | "password";
+  type?: "text" | "number" | "password";
 }>();
 </script>
 
 <template>
-  <input :type="type" v-model="model" />
+  <input :type="type" v-model="model" class="bg-gray-300 p-3 font-bold" />
 </template>
