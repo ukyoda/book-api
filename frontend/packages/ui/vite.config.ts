96dd0b302b01c2d8efaf121926282da0dd45e373
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-26 19:11:57 +0900

    fix

--- frontend/packages/ui/vite.config.ts
+++ frontend/packages/ui/vite.config.ts
@@ -3,12 +3,14 @@ import { URL, fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), libInjectCss()],
   build: {
     lib: {
       entry: [
+        fileURLToPath(new URL("src/index.ts", import.meta.url)),
         fileURLToPath(new URL("src/Button/index.ts", import.meta.url)),
         fileURLToPath(new URL("src/TextInput/index.ts", import.meta.url)),
         fileURLToPath(new URL("src/List/index.ts", import.meta.url)),
