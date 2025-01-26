ab93f05ae153a27ca57f1757307064023af75213
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-26 11:56:49 +0900

    fix

--- frontend/packages/ui/src/TextInput/index.stories.ts
+++ frontend/packages/ui/src/TextInput/index.stories.ts
@@ -5,7 +5,7 @@ import TextInput from "./index.vue";
 type Story = StoryObj<typeof meta>;
 const meta = {
   title: "@repo/ui/TextInput",
-  component: TextInput,
+  component: TextInput as Record<keyof typeof TextInput, unknown>,
   tags: ["autodocs"],
 } satisfies Meta<typeof TextInput>;
 
