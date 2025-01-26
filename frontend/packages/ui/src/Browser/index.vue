5a7548b1abd908e69b90215491339e970595df48
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-27 02:09:36 +0900

    fix

--- frontend/packages/ui/src/Browser/index.vue
+++ frontend/packages/ui/src/Browser/index.vue
@@ -1,4 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+import Browser from "./index.vue";
+import { type BrowsingData } from "./type";
+
+const { items } = defineProps<{
+  items: BrowsingData[];
+}>();
+const emits = defineEmits<{
+  click: [value: BrowsingData];
+}>();
+
+const nextPage = ref<BrowsingData[]>();
+const onClick = (item: BrowsingData) => {
+  if (item.children) {
+    nextPage.value = item.children;
+  } else {
+    nextPage.value = undefined;
+    emits("click", item);
+  }
+};
+</script>
 <template>
-  <div>aaa</div>
+  <div>
+    <div v-for="(item, idx) in items" :key="idx">
+      <button type="button" @click="onClick(item)">
+        {{ item.label }}
+      </button>
+    </div>
+  </div>
+  <Browser v-if="nextPage" :items="nextPage" />
 </template>
