bfaf97716981995f11316aaeedd9497e3b3b63ef
ukyoda <3340595+ukyoda@users.noreply.github.com>
2025-01-27 00:08:55 +0900

    fix

--- frontend/packages/tailwindcss/tailwind.config.ts
+++ frontend/packages/tailwindcss/tailwind.config.ts
@@ -8,7 +8,6 @@ const config: Omit<Config, "content"> = {
         primary: "#4CAF50", // 落ち着いた緑 (清潔感＆安心感)
         secondary: "#2196F3", // 落ち着いた青 (視認性向上)
         accent: "#FFC107", // 柔らかい黄色 (ポイント的な使い方)
-        normal: "#F5F5F5", // 背景色 (白っぽいグレー)
         dark: "#212121", // 文字色やヘッダーなどで使う濃いグレー
         danger: "#F44336", // 警告色 (赤)
         link: "#2196F3", // リンク色 (青)
--- frontend/packages/ui/src/Button/index.vue
+++ frontend/packages/ui/src/Button/index.vue
@@ -33,10 +33,12 @@ const themeStyle = computed(() => {
   const key = `${variant}-${color}`;
   return (
     {
+      // SOLID
       "solid-primary": "bg-primary text-white rounded-md",
       "solid-secondary": "bg-secondary text-white rounded-md",
       "solid-danger": "bg-danger text-white rounded-md",
-      "solid-normal": "bg-normal text-black border border-gray-400 rounded-md",
+      "solid-normal": "bg-gray-200 text-black rounded-md",
+      // OUTLINE
       "outline-primary":
         "bg-transparent text-primary border border-primary rounded-md",
       "outline-secondary":
@@ -44,11 +46,12 @@ const themeStyle = computed(() => {
       "outline-danger":
         "bg-transparent text-danger border border-danger rounded-md",
       "outline-normal":
-        "bg-transparent text-black border border-black rounded-md",
-      "ghost-primary": "text-blue-500",
-      "ghost-secondary": "text-gray-500",
-      "ghost-danger": "text-red-500",
-      "ghost-normal": "text-gray-500",
+        "bg-transparent text-gray-600 border border-gray-600 rounded-md",
+      // GHOST
+      "ghost-primary": "text-primary rounded-md hover:bg-gray-100",
+      "ghost-secondary": "text-secondary rounded-md hover:bg-gray-100",
+      "ghost-danger": "text-danger rounded-md hover:bg-gray-100",
+      "ghost-normal": "text-black rounded-md hover:bg-gray-100",
     } satisfies Record<`${ButtonVariant}-${ButtonTheme}`, string>
   )[key];
 });
